edda630422b3bb7fe5fbfd9ac2fbe81608f3f14a
sunsss123 <81277071+sunsss123@users.noreply.github.com>
2021-05-24 20:04:04 +0900

    [최종 수정]

--- numberbaseballgame.py
+++ numberbaseballgame.py
@@ -7,29 +7,29 @@ import math
 
 # 초기화
 cnt = 0
-guess = [] #입력값을 배열로 저장
-strikecnt = 0 #스트라이크 횟수
+guess = []
+strikecnt = 0
 
-isPlayer1 = True #플레이어 1이 게임중
-isPlayer2 = False #플레이어 2는 참여불가
+isPlayer1 = True
+isPlayer2 = False
 
-player1Base = 0 #플레이어1의 베이스위치
-player2Base = 0 #플레이어2의 베이스위치
+player1Base = 0
+player2Base = 0
 
-tmpPlayer1Ball = 0 #플레이어1의 누적 볼 수
-tmpPlayer2Ball = 0 #플레이어2의 누적 볼 수 
+tmpPlayer1Ball = 0
+tmpPlayer2Ball = 0
 
-cntPlayer1 = 0 #플레이어1의 점수 카운트
-cntPlayer2 = 0 #플레이어2의 점수 카운트
+cntPlayer1 = 0
+cntPlayer2 = 0
 
 # 게임진행
-while cntPlayer1 < 3 and cntPlayer2 < 3: #플레이어 1과 2중 누가 먼저 3점을 먹었냐
+while cntPlayer1 < 3 and cntPlayer2 < 3:
     strikecnt = 0
     ballcnt = 0
-    guess = [] #모두 빈 배열로 초기화
+    guess = []
 
-    answer = random.sample(range(1, 10), 4) #랜덤 수
-    #print("정답은=", answer) #테스트용
+    answer = random.sample(range(1, 10), 4)
+    print("정답은=", answer)
 
     for i in range(4):
         input_num = int(input("{}, 1~9까지 숫자를 입력하세요:".format(i)))
@@ -46,33 +46,42 @@ while cntPlayer1 < 3 and cntPlayer2 < 3: #플레이어 1과 2중 누가 먼저 3
     if isPlayer1 == True:
         if tmpPlayer1Ball + ballcnt > 0 or strikecnt > 0:
             if (tmpPlayer1Ball + ballcnt) / 4 > 0:
-                player1Base += math.trunc((tmpPlayer1Ball + ballcnt) / 4)
-                # print("베이스 : {}".format(player1Base))
+                player1Base = player1Base + math.trunc((tmpPlayer1Ball + ballcnt) / 4)
+                # print("베이스1 : {}".format(player1Base))
                 tmpPlayer1Ball = (tmpPlayer1Ball + ballcnt) % 4
             if strikecnt > 0:
-                player1Base = + strikecnt
+                player1Base = player1Base + strikecnt
+                # print("베이스2 : {}".format(player1Base))
             if player1Base >= 4:
-                cntPlayer1 += math.trunc(player1Base / 4)
+                cntPlayer1 = cntPlayer1 + math.trunc(player1Base / 4)
                 player1Base = player1Base % 4
+                # print("베이스3 : {}".format(player1Base))
 
-        print("Player1 -> 베이스 = {:g}, 스트라이크 = {:g}, 볼 = {:g}, stack_볼 = {:g},  점수 = {:g}".format(
-            player1Base, strikecnt, ballcnt, tmpPlayer1Ball, cntPlayer1))
+        print("Player1 -> 베이스 = {:g}, 스트라이크 = {:g}, 볼 = {:g}, stack_볼 = {:g},  점수 = {:g}".format(player1Base, strikecnt,
+                                                                                                 ballcnt,
+                                                                                                 tmpPlayer1Ball,
+                                                                                                 cntPlayer1))
         isPlayer1 = False
         isPlayer2 = True
+
     elif isPlayer2 == True:
         if tmpPlayer2Ball + ballcnt > 0 or strikecnt > 0:
             if (tmpPlayer2Ball + ballcnt) / 4 > 0:
-                player2Base += math.trunc((tmpPlayer2Ball + ballcnt) / 4)
-                # print("베이스 : {}".format(player1Base))
+                player2Base = player2Base + math.trunc((tmpPlayer2Ball + ballcnt) / 4)
+                # print("베이스1 : {}".format(player1Base))
                 tmpPlayer2Ball = (tmpPlayer2Ball + ballcnt) % 4
             if strikecnt > 0:
-                player2Base = + strikecnt
+                player2Base = player2Base + strikecnt
+                # print("베이스2 : {}".format(player1Base))
             if player2Base >= 4:
-                cntPlayer2 += math.trunc(player2Base / 4)
+                cntPlayer2 = cntPlayer2 + math.trunc(player2Base / 4)
                 player2Base = player2Base % 4
+                # print("베이스3 : {}".format(player1Base))
 
-        print("Player2 -> 베이스 = {:g}, 스트라이크 = {:g}, 볼 = {:g}, stack_볼 = {:g},  점수 = {:g}".format(
-            player2Base, strikecnt, ballcnt, tmpPlayer2Ball, cntPlayer2))
+        print("Player2 -> 베이스 = {:g}, 스트라이크 = {:g}, 볼 = {:g}, stack_볼 = {:g},  점수 = {:g}".format(player2Base, strikecnt,
+                                                                                                 ballcnt,
+                                                                                                 tmpPlayer2Ball,
+                                                                                                 cntPlayer2))
         isPlayer1 = True
         isPlayer2 = False
 
