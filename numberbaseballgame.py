e48986702b34ee26f2825c949064633cea9be328
sunsss123 <81277071+sunsss123@users.noreply.github.com>
2021-05-24 20:20:15 +0900

    [최종 수정(진)]

--- numberbaseballgame.py
+++ numberbaseballgame.py
@@ -7,20 +7,20 @@ import math
 
 # 초기화
 cnt = 0
-guess = []
-strikecnt = 0
+guess = [] #입력값을 배열로 저장
+strikecnt = 0 #스트라이크 횟수
 
-isPlayer1 = True
-isPlayer2 = False
+isPlayer1 = True #플레이어 1이 게임중
+isPlayer2 = False #플레이어 2는 참여불가
 
-player1Base = 0
-player2Base = 0
+player1Base = 0 #플레이어1의 베이스위치
+player2Base = 0 #플레이어2의 베이스위치
 
-tmpPlayer1Ball = 0
-tmpPlayer2Ball = 0
+tmpPlayer1Ball = 0 #플레이어1의 누적 볼 수
+tmpPlayer2Ball = 0 #플레이어2의 누적 볼 수
 
-cntPlayer1 = 0
-cntPlayer2 = 0
+cntPlayer1 = 0 #플레이어1의 점수 카운트
+cntPlayer2 = 0 #플레이어2의 점수 카운트
 
 # 게임진행
 while cntPlayer1 < 3 and cntPlayer2 < 3:
@@ -29,7 +29,7 @@ while cntPlayer1 < 3 and cntPlayer2 < 3:
     guess = []
 
     answer = random.sample(range(1, 10), 4)
-    print("정답은=", answer)
+    #print("정답은=", answer) #원만한 게임진행을 위한 출력문 주석처리
 
     for i in range(4):
         input_num = int(input("{}, 1~9까지 숫자를 입력하세요:".format(i)))
@@ -43,7 +43,7 @@ while cntPlayer1 < 3 and cntPlayer2 < 3:
 
     print("strike = {}, ball = {}".format(strikecnt, ballcnt))
 
-    if isPlayer1 == True:
+    if isPlayer1 == True: #플레이어1의 게임진행
         if tmpPlayer1Ball + ballcnt > 0 or strikecnt > 0:
             if (tmpPlayer1Ball + ballcnt) / 4 > 0:
                 player1Base = player1Base + math.trunc((tmpPlayer1Ball + ballcnt) / 4)
@@ -61,10 +61,10 @@ while cntPlayer1 < 3 and cntPlayer2 < 3:
                                                                                                  ballcnt,
                                                                                                  tmpPlayer1Ball,
                                                                                                  cntPlayer1))
-        isPlayer1 = False
-        isPlayer2 = True
+        isPlayer1 = False #플레이어1 게임 참여불가 상태
+        isPlayer2 = True #플레이어2 게임진행
 
-    elif isPlayer2 == True:
+    elif isPlayer2 == True: #플레이어2
         if tmpPlayer2Ball + ballcnt > 0 or strikecnt > 0:
             if (tmpPlayer2Ball + ballcnt) / 4 > 0:
                 player2Base = player2Base + math.trunc((tmpPlayer2Ball + ballcnt) / 4)
@@ -82,8 +82,8 @@ while cntPlayer1 < 3 and cntPlayer2 < 3:
                                                                                                  ballcnt,
                                                                                                  tmpPlayer2Ball,
                                                                                                  cntPlayer2))
-        isPlayer1 = True
-        isPlayer2 = False
+        isPlayer1 = True #플레이어1 게임진행
+        isPlayer2 = False #플레이어2 게임 참여불가 상태
 
     if cntPlayer1 == 3:
         print("player1이 승리하였습니다.")
